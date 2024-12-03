d92e9c5de70bdbe555aae89df62ad7f535ad5bec
Victor <onetworacks@gmail.com>
2024-12-03 13:16:18 +0000

    .THEN METHOD

--- java.js
+++ java.js
@@ -1,4 +1,4 @@
-//API we want to hit - you have to wrap it in a fetch 
+//API we want to hit - you have to wrap it in a fetch
 
 // fetch("https://jsonplaceholder.typicode.com/users/1"))
 
@@ -11,17 +11,28 @@
  * now this is giving us the data but its giving it to locked still inside the promise,
  *  to unlock the promise you have to use response.json().then
  * inside of .then() is a callback
- * 
- * the first promise is what we are fetching from the backend 
+ *
+ * the first promise is what we are fetching from the backend
  * and then in order to make it compatible with the frontend so you can use
- * with javscript you have to use response.json 
+ * with javscript you have to use response.json
  * response.json() is a promise within itself
  * .then method is used to unlock the promise
  */
-fetch("https://jsonplaceholder.typicode.com/users/1").then(response => {
-    response.json().then(data => {
-        console.log(data)
-    })
-})
 
-//
+/**
+ * fetch("https://jsonplaceholder.typicode.com/users/1")
+  .then((response) => {
+    return response.json();
+  })
+  .then((response) => {
+    console.log(response);
+  });
+ * 
+ * 
+ * */ 
+
+//when you return a promise inside of a .then - the WHOLE fetch becomes a response.json()
+// therefore we need to unlock the entire thing frm there and we do that by using .then and a callback
+
+
+// Async/Await method //
